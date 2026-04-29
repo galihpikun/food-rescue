1764991082547ff115b3d6193ae9c59111c177ca
reza aditya <rezaadityaa26@gmail.com>
2026-04-29 22:45:53 +0700

    add: route for getorder and updateorderstatus on order route

--- src/routes/orderRoute.js
+++ src/routes/orderRoute.js
@@ -1,9 +1,13 @@
 import express from "express";
-import { createOrder } from "../controllers/orderController";
+import { createOrder, getOrders } from "../controllers/orderController";
 import { jwtMiddleware } from "../middlewares/authMiddleware";
 
 const routeOrder = express.Router();
 
 routeOrder.post('/', jwtMiddleware, createOrder);
+
+routeOrder.get('/', jwtMiddleware, getOrders);
+
+routeOrder.put('/:id/status', jwtMiddleware, updateOrderStatus);
 
 export default routeOrder;
